871390a0e3cd1d80a41d9bf2a84f7d13832517bb
jiang-111 <1051584597@qq.com>
2026-05-27 03:58:30 +0800

    Set initial release version to 0.1.0

--- content/scripts/chat-window.js
+++ content/scripts/chat-window.js
@@ -261,7 +261,7 @@
       clientInfo: {
         name: "zotero_codex_chat",
         title: "Zotero Codex Chat",
-        version: "0.2.13",
+        version: "0.1.0",
       },
       capabilities: {
         experimentalApi: true,
